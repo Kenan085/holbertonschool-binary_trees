6a4318e699aa7778429a51825720567752b26651
Kenan <kenanshafizade85@gmail.com>
2024-04-19 18:24:11 +0000

    Task 16 done

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,19 +23,38 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree.
+ * Return: Number of leaves. If the tree is NULL, return 0.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
+}
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the three
  * Return: 1 if the tree is perfect, otherwise 0.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t check, height;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (left_height == right_height);
+	height = binary_tree_height(tree);
+	check = (1 << (height + 1)) - 1;
+	if (check == binary_tree_leaves(tree))
+		return (1);
+	return (0);
+
 }
+
