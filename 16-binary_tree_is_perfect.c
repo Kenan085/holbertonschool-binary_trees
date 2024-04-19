8e63a50ed3206e62f13e5640e49dde7a0068c632
Kenan <kenanshafizade85@gmail.com>
2024-04-19 18:05:28 +0000

    Task 16 done

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,5 +37,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (binary_tree_is_full(tree) && left_height == right_height);
+	return (left_height == right_height);
 }
